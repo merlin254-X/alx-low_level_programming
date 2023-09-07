02820223b7475791c1fc35697f3038a2b5959e4f
merlin254-X <merlinderick8@gmail.com>
2023-09-07 10:19:08 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-entry point
+ * Description: a C program that print with put function
+ * Return: return value is 0
+ *
+ * /
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}	
