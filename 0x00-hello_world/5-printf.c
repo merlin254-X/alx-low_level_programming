e6d4b33bf76611d302eb10970b35819f796c6543
merlin254-X <merlinderick8@gmail.com>
2023-09-07 11:27:33 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function
+ * Return:  Always 0 success
+ *
+ */
+
+int main(void)
+{
+printif("with proper grammar, but the outcome is a piece of art.\n");
+return (0);
+}
