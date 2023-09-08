184ebe1061e616b0e45e39815871a364f4963e1a
merlin254-X <merlinderick8@gmail.com>
2023-09-08 09:07:04 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,16 +16,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-{
-print("%d is positive\n", n);
-}
-if else(n == 0)
-}
-print("%d is zero\n", n);
-{
+
+printf("%d is positive\n", n);
+
+else if (n == 0)
+
+printf("%d is zero\n", n);
+
 else
-}
-print("%d is negative\n", n);
-{
+
+printf("%d is negative\n", n);
+
 return (0);
 }
