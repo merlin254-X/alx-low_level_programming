35aeaeaf56719968b71200ae4b94c1f84c54bb86
merlin254-X <merlinderick8@gmail.com>
2023-09-10 03:43:10 -0700

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: return 1 successiful
+ */
+
+int main(void)
+{
+char *message =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr, "%s", message);
+return (1);
+}
