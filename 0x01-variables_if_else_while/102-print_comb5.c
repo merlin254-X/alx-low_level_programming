d85a678993c85aaa7eada32c0584ed2c130fb1fd
merlin254-X <merlinderick8@gmail.com>
2023-09-10 02:35:53 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: Always return 0
+ *
+ */
+
+
+int putchar(int c);
+
+int main(void)
+{
+int i, j, first_digit_i, second_digit_i, first_digit_j, second_digit_j;
+
+for (i = 0; i <= 99; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+first_digit_i = i / 10;
+second_digit_i = i % 10;
+first_digit_j = j / 10;
+second_digit_j = j % 10;
+
+putchar(first_digit_i + '0');
+putchar(second_digit_i + '0');
+putchar(' ');
+putchar(first_digit_j + '0');
+putchar(second_digit_j + '0');
+
+if (!(i == 98 && j == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
