633aa7f3f5cde5be3df56b48c49f6f94cf33b307
merlin254-X <merlinderick8@gmail.com>
2023-09-12 02:25:47 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - this the main function
+ * Return: Always return
+ *
+ */
+
+void print_alphabet(void)
+
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+
+}
+
