ec7c3fd659a5387e509d85d8455680836f745b86
merlin254-X <merlinderick8@gmail.com>
2023-09-14 13:40:20 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  * _isdigit - function that checks for a digit (0 through 9)
  * @c: parameter to cieck
- * Return: 1 1 if c is a digit 0 otherwise
+ * Return: 1 if c is a digit 0 otherwise
  *
  */
 
