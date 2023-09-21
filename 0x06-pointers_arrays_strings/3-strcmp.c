5d7d3b6d570826d0a270040b8ecc0ad5ab1fd208
merlin254-X <merlinderick8@gmail.com>
2023-09-21 12:38:27 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+* _strcmp-comparestwostrings.
+* @s1:firststring
+* @s2:secondstring
+* Return:s1[i]-s2[i]
+*/
+
+int _strcmp(char *s1, char *s2)
+
+{
+
+int i;
+
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+
+}
+i++;
+}
+
+return (0);
+
+}
