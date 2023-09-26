906594bfa99652b29c7ec883df2c6947190df249
merlin254-X <merlinderick8@gmail.com>
2023-09-26 14:02:23 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,24 +8,26 @@
  * Return: A pointer to the first occurrence of the character c in s,
  * or NULL if the character is not found.
  *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 char *_strchr(char *s, char c)
 {
-if (c == '\0')
-{
-return (s);
-}
-
 while (*s != '\0')
 {
 if (*s == c)
 {
 return (s);
-
 }
 s++;
 }
-
 return (NULL);
 }
 
