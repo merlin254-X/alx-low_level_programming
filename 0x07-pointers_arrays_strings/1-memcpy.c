c5e572f3632f31d8383eb48170c7a08b1a8d9429
merlin254-X <merlinderick8@gmail.com>
2023-09-26 13:02:03 -0700

    strchr

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,23 +3,19 @@
 /**
  * _memcpy - function copies n bytes from
  * memory area src to memory area dest
- * @dest: memory area
- * @src: memory area
- * @n: bytes
+ * @dest: memory area where is stored
+ * @src: memory area where is copied
+ * @n: number of bytes
  * Return: Returns a pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	{
-		for (i = 0; i < n; i++)
-
-		{
-			*(dest + 1) = *(src + 1);
-		}
-
-	}
-	return (dest);
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
