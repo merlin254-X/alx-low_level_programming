18c3dd0961d2f0d0aab8be4add9abc92f08f3c87
merlin254-X <merlinderick8@gmail.com>
2023-09-29 17:13:17 -0700

    program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,49 +2,39 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point of the program
- * @argc: The number of command-line arguments passed to the program
- * @argv: An array of strings containing the command-line arguments
+ * main - Adds positive numbers
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
  *
- * Description: This program adds positive numbers and prints the result,
- * followed by a new line. If no numbers are passed, it prints 0 followed
- * by a new line. If one of the numbers contains symbols that are not digits,
- * it prints "Error" followed by a new line and returns 1 to indicate an error.
- *
- * Return: 0 on success, 1 on error
+ * Return: 0 if no number is passed, 1 if an
+ * error occurs, or the sum of the numbers
  */
 int main(int argc, char *argv[])
 {
-int i;
-int num = 0;
+	int i, num;
+	int sum = 0;
 
-if (argc < 2)
-{
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-printf("0\n");
-return (0);
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+
+		if (num == 0 && argv[i][0] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		if (num > 0)
+			sum += num;
+	}
+
+	printf("%d\n", sum);
+	return (0);
 }
 
-for (i = 1; i < argc; i++)
-{
-
-num = atoi(argv[i]);
-
-if (num == 0 && argv[i][0] != '0')
-
-
-printf("Error\n");
-return (1);
-}
-
-if (num > 0)
-{
-
-num += num;
-}
-
-
-printf("%d\n", num);
-
-return (0);
-}
