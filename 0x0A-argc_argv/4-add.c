97e39addffff20e8f67dbfbaf1b4ea5a03d2316b
merlin254-X <merlinderick8@gmail.com>
2023-09-29 17:32:24 -0700

    program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	{
 		num = atoi(argv[i]);
 
-		if (num == 0 && argv[i][0] != '0')
+		if (num == (0 && argv[i][0] != '0') || (num < 0))
 		{
 			printf("Error\n");
 			return (1);
