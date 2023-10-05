cf9dca0635fef7a1ec7214d6497585feb309d573
merlin254-X <merlinderick8@gmail.com>
2023-10-05 10:52:19 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,24 +16,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 		if (s1 == NULL)
 		{
-				s1 = "";
+			s1 = "";
 		}
 		if (s2 == NULL)
 		{
-				s2 = "";
+			s2 = "";
 		}
 		s1_len = strlen(s1);
 		s2_len = strlen(s2);
 
 		if (n >= s2_len)
 		{
-				n = s2_len;
+			n = s2_len;
 		}
 		result = malloc(s1_len + n + 1);
 
 		if (result == NULL)
 		{
-				return (NULL);
+			return (NULL);
 		}
 		memcpy(result, s1, s1_len);
 		memcpy(result + s1_len, s2, n);
