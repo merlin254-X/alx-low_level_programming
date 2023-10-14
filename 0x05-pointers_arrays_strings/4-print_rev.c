9d4ced4f8a0723f0d2ff4cc040c510335b15150c
merlin254-X <merlinderick8@gmail.com>
2023-10-14 10:50:22 -0700

    reversing a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * print_rev -	prints a string, in reverse,
+ * followed by a new line.
+ * @s: string to be reversed
+ * Return: nothing
+ *
+ */
+
+
+void print_rev(char *s)
+{
+	if (*s == '\0')
+		return;
+	print_rev(s + 1);
+	putchar(*s);
+}
