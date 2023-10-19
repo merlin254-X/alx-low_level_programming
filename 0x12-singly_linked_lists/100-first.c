f8874809ceca96574118aab2fac0c3440f4908df
merlin254-X <merlinderick8@gmail.com>
2023-10-19 14:38:16 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * customMessage - Prints a custom message to the standard output.
+ *
+ */
+void customMessage(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
