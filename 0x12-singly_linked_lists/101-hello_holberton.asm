c90c5c061b74e3b30f76571fdc6775a6e1c6dd99
merlin254-X <merlinderick8@gmail.com>
2023-10-19 17:07:10 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,5 @@
 section .data
-    hello db "Hello, Holberton", 10, 0
+    hello db 'Hello, Holberton', 10, 0
 
 section .text
     global main
@@ -16,4 +16,3 @@ main:
     mov rsp, rbp
     pop rbp
     ret
-
