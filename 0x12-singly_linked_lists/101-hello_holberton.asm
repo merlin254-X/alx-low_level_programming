045ae87500c20487121a01ed4f0445e1e9e17d7c
merlin254-X <merlinderick8@gmail.com>
2023-10-20 13:44:33 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,12 @@
-section .data
-    msg db "Hello, Holberton\n", 0
+global main
 
-section .text
-    global main
+      extern printf
+
 main:
-    ; Set up the arguments for the printf function.
-    mov rax, 1 ; system call number for printf
-    mov rdi, 1 ; file handle for stdout
-    mov rsi, msg ; address of the message to print
-    mov rdx, 17 ; length of the message
+      mov edi, format
+      xor eax, eax
+      call printf
+      mov eax, 0
+      ret
 
-    ; Call the printf function.
-    syscall
-
-    ; Exit the program.
-    mov rax, 60 ; system call number for exit
-    xor rdi, rdi ; exit code
-    syscall
-
+format: db "Hello, Holberton\n", 0
