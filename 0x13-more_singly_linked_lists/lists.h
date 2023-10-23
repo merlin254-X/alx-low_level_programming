cc3da9904a1e2faa505be4a6931eae05e6829a06
merlin254-X <merlinderick8@gmail.com>
2023-10-23 11:23:26 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
-
 #define LISTS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -12,14 +15,10 @@
  *
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
