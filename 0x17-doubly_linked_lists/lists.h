5887b2889b55cc14c86efbc9c0e03c7eb2997e54
merlin254-X <merlinderick8@gmail.com>
2023-12-07 22:09:13 +0300

    task 1

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define LISTS_H
 
 #include <stdio.h>
-
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -20,5 +20,5 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-
+size_t dlistint_len(const dlistint_t *h);
 #endif
